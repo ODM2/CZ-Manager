e459112803705eed97c06f91afbbb1a172f6179b
Miguel Leon <leonmi@sas.upenn.edu>
2015-05-15 13:13:06 -0400

    fix for file clean up.

--- odm2testsite/urls.py
+++ odm2testsite/urls.py
@@ -8,6 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
 	 url(r'^odm2testapp/', include('odm2testapp.urls')),
 )
