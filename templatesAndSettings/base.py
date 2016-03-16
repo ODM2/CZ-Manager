f96152efd0b6a462461b2d70518ad87b63ac5657
Miguel Leon <leonmi@sas.upenn.edu>
2016-03-16 11:49:16 -0400

    Decided to change the site index some, this removes repeated code in my_index, just using the app_index for the list of all models to simplify the code.

--- templatesAndSettings/base.py
+++ templatesAndSettings/base.py
@@ -80,7 +80,7 @@ ADMIN_SHORTCUTS = [
 
         'shortcuts': [
             {
-                'url': '/'+URL_PATH,
+                'url': CUSTOM_TEMPLATE_PATH,
                 'app_name': 'ODM2CZOData',
                 'title': 'Home',
                 'class':'home',
