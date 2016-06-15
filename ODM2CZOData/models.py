2b51e7f56c544fe117e8c49669b6fec308510c09
Miguel Leon <leonmi@sas.upenn.edu>
2016-06-15 15:38:21 -0400

    Add another date time format for importing dataloggerfiles

--- ODM2CZOData/models.py
+++ ODM2CZOData/models.py
@@ -96,8 +96,12 @@ def process_datalogger_file(f,fileid, databeginson,columnheaderson):
                             dateT = time.strptime(row[0],"%m/%d/%Y %H:%M:%S")#'1/1/2013 0:10
                             datestr = time.strftime("%Y-%m-%d %H:%M:%S",dateT)
                         except ValueError:
-                            dateT = time.strptime(row[0],"%Y-%m-%d %H:%M:%S.%f")#'1/1/2013 0:10
-                            datestr = time.strftime("%Y-%m-%d %H:%M:%S",dateT)
+                            try:
+                                dateT = time.strptime(row[0],"%Y-%m-%d %H:%M:%S")#'1/1/2013 0:10
+                                datestr = time.strftime("%Y-%m-%d %H:%M:%S",dateT)
+                            except ValueError:
+                                dateT = time.strptime(row[0],"%Y-%m-%d %H:%M:%S.%f")#'1/1/2013 0:10
+                                datestr = time.strftime("%Y-%m-%d %H:%M:%S",dateT)
                     #for each column in the data table
                     #raise ValidationError("".join(str(rowColumnMap)))
                     for colnum in rowColumnMap:
