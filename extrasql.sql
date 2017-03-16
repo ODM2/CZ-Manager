5b7d770c186ff4e8652198d3066df7fe89520f81
Miguel Leon <leonmi@sas.upenn.edu>
2017-03-16 16:14:17 -0400

    existing sql for the timeseriesresultvaluesextwannotations wasn't working for some reason. This sql works.

--- extrasql.sql
+++ extrasql.sql
@@ -129,35 +129,34 @@ ALTER TABLE odm2extra.timeseriesresultvaluesext
   OWNER TO postgres;
 
 CREATE OR REPLACE VIEW odm2extra.timeseriesresultvaluesextwannotations AS 
- SELECT tsrv.valueid,
-    tsrv.datavalue,
-    tsrv.valuedatetime,
-    tsrv.valuedatetimeutcoffset,
-    tsrv.censorcodecv,
-    tsrv.qualitycodecv,
-    tsrv.timeaggregationinterval,
-    tsrv.timeaggregationintervalunitsid,
+ SELECT timeseriesresultvalues.valueid,
+    timeseriesresultvalues.datavalue,
+    timeseriesresultvalues.valuedatetime,
+    timeseriesresultvalues.valuedatetimeutcoffset,
+    timeseriesresultvalues.censorcodecv,
+    timeseriesresultvalues.qualitycodecv,
+    timeseriesresultvalues.timeaggregationinterval,
+    timeseriesresultvalues.timeaggregationintervalunitsid,
     samplingfeatures.samplingfeaturename,
-    samplingfeatures.samplingfeaturetypecv,
     processinglevels.processinglevelcode,
     variables.variablecode,
     units.unitsabbreviation,
-    tsrv.resultid,
+    timeseriesresultvalues.resultid,
     cv_aggregationstatistic.name AS aggregationstatisticname,
-    b.annotationtext
-   FROM odm2.timeseriesresultvalues tsrv
-     LEFT JOIN odm2.timeseriesresultvalueannotations a ON tsrv.valueid = a.valueid
-     LEFT JOIN odm2.annotations b ON a.annotationid = b.annotationid,
-    odm2.results,
+    annotations.annotationtext
+   FROM odm2.timeseriesresultvalues,
     odm2.timeseriesresults,
+    odm2.results,
     odm2.featureactions,
     odm2.samplingfeatures,
     odm2.processinglevels,
     odm2.variables,
     odm2.units,
-    odm2.cv_aggregationstatistic
-  WHERE tsrv.resultid = timeseriesresults.resultid AND timeseriesresults.resultid = results.resultid AND timeseriesresults.aggregationstatisticcv::text = cv_aggregationstatistic.name::text AND results.featureactionid = featureactions.featureactionid AND results.processinglevelid = processinglevels.processinglevelid AND results.variableid = variables.variableid AND results.unitsid = units.unitsid AND featureactions.samplingfeatureid = samplingfeatures.samplingfeatureid
-  ORDER BY tsrv.datavalue DESC;
+    odm2.cv_aggregationstatistic,
+    odm2.timeseriesresultvalueannotations,
+    odm2.annotations
+  WHERE timeseriesresultvalues.resultid = timeseriesresults.resultid AND timeseriesresultvalues.valueid = timeseriesresultvalueannotations.valueid AND timeseriesresults.resultid = results.resultid AND timeseriesresults.aggregationstatisticcv::text = cv_aggregationstatistic.name::text AND results.featureactionid = featureactions.featureactionid AND results.processinglevelid = processinglevels.processinglevelid AND results.variableid = variables.variableid AND results.unitsid = units.unitsid AND featureactions.samplingfeatureid = samplingfeatures.samplingfeatureid AND timeseriesresultvalueannotations.annotationid = annotations.annotationid
+  ORDER BY timeseriesresultvalues.datavalue DESC;
 
 ALTER TABLE odm2extra.timeseriesresultvaluesextwannotations
   OWNER TO postgres;
