dcac5e144a2f7d13a6ce9756bb5c142e1125621d
Miguel Leon <leonmi@sas.upenn.edu>
2017-04-19 16:55:06 -0400

    Revert "revert docker changes in last commit"
    This reverts commit 64c9b0a56a9f4b1fd724fb576b67434a86c3f472.

--- docker/README.md
+++ docker/README.md
@@ -5,4 +5,4 @@ Requirements to run [docker image](https://hub.docker.com/r/lsetiawan/odm2admin/
 1. ODM2 Database backup sql for PostgreSQL called odm2admindb.backup.
 2. Docker installed on Linux or MacOS, currently not working on windows.
 To run:
-$ docker run -d -p 8010:8010 -v path/to/local/db/backup/folder/:/db/ lsetiawan/odm2admin:latest
+$ docker run -d -p 8010:8010 -v /path/to/local/db/backup/folder/:/db/ lsetiawan/odm2admin:latest
--- docker/startup.sh
+++ docker/startup.sh
@@ -4,8 +4,8 @@ echo "Activating environment..."
 source activate odm2adminenv
 
 echo "Building database..."
-su - postgres -c 'pg_restore -d odm2_db -1 -v "/db/odm2admindb.backup"'
+su - postgres -c 'pg_restore -d odm2_db -1 -v "../ODM2AdminExamplePostgresqlDB"'
 su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'test';\""
 
 echo "Running server..."
-python /ODM2-Admin/manage.py runserver 0.0.0.0:8010
+python /ODM2-Admin/manage.py runserver 127.0.0.1:8010
