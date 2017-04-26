7447ef1434abbe7599702acfe9e55d3c85fe23b9
lsetiawan <landungs@uw.edu>
2017-04-26 11:07:46 -0700

    Automatically migrate Django tables on startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,5 +8,8 @@ su - postgres -c 'pg_restore -d odm2_db -1 -v "/ODM2-Admin/ODM2AdminDBBlank"'
 
 su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'test';\""
 
+echo "Migrate django tables"
+python /ODM2-Admin/manage.py migrate
+
 echo "Running server..."
 python /ODM2-Admin/manage.py runserver 0.0.0.0:8010
