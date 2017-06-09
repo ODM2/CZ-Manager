b1fe2880f25bf9f44ee5b4893a7b4e205958fb37
Miguel Leon <leonmi@sas.upenn.edu>
2017-06-09 11:58:23 -0400

    revert some changes to docker files. fix bad urls in shortcuts.

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,12 +2,14 @@
 
 echo "Activating environment..."
 source activate odm2adminenv
-conda install --yes -c conda-forge pytz
 
 echo "Building database..."
 su - postgres -c 'pg_restore -d odm2_db -1 -v "/ODM2-Admin/ODM2AdminDBBlank"'
 
 su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'test';\""
+
+echo "Migrate django tables"
+python /ODM2-Admin/manage.py migrate
 
 echo "Running server..."
 python /ODM2-Admin/manage.py runserver 0.0.0.0:8010
