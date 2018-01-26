b7aa6f6d892e8ca19ba04845bc764fe37e73e304
Miguel Leon <leonmi@sas.upenn.edu>
2018-01-26 12:55:14 -0500

    create sql db for travis and testing

--- templatesAndSettings/settings/development.py
+++ templatesAndSettings/settings/development.py
@@ -9,9 +9,12 @@ from .base import *
 DEBUG = True
 """ END DEBUG CONFIGURATION """
 
-""" EXPORTDB FLAG CONFIGURATION - if set to true this will use Camel case table names for SQLite"""
-EXPORTDB = False 
-""" EXPORTDB FLAG CONFIGURATION """
+
+""" TRAVIS CONFIGURATION """
+TRAVIS_ENVIRONMENT = False
+if 'TRAVIS' in os.environ:
+    TRAVIS_ENVIRONMENT = True
+""" END TRAVIS CONFIGURATION """
 
 """ ALLOWED HOSTS CONFIGURATION """
 ALLOWED_HOSTS = ['127.0.0.1',]
@@ -29,21 +32,33 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 123
 """ EMAIL CONFIGURATION """
 
-
 """ DATABASE CONFIGURATION """
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'db_name',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=public,admin,odm2,odm2extra'
+if TRAVIS_ENVIRONMENT:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgis',
+            'NAME': 'test',  # Must match travis.yml setting
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
         }
     }
-}
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': 'db_name',
+            'USER': 'user',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'OPTIONS': {
+                'options': '-c search_path=public,admin,odm2,odm2extra'
+            }
+        }
+    }
 """ END DATABASE CONFIGURATION """
 
 """ SENSOR DASHBOARD CONFIGURATION """
@@ -76,7 +91,6 @@ MAP_CONFIG = {
 DATA_DISCLAIMER = {
     "text" : "Add a link discribing where your data come from",
     "linktext" : "The name of my site",
-    "link" : "http://mysiteswegpage.page/"
-
+    "link" : "http://mysiteswegpage.page/",
 }
 """ END DATA DISCLAIMER CONFIGURATION """
